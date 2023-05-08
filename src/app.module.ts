453b262d7df6ee63ec85908e38b79353d221e79c
macluiggy <luiggy.macias@imagineapps.co>
2023-05-08 18:58:56 -0500

    config microservice passenger

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PassengerModule } from './passenger/passenger.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PassengerModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development'],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }),
+    PassengerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
